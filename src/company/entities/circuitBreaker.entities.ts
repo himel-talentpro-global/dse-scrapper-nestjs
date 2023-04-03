81c2fbd88a192f7df58a348d587ec219b2907e3d
Zahid Hasan <dihaz.nett@gmail.com>
2023-04-03 13:55:34 +0600

    ' '

--- src/company/entities/circuitBreaker.entities.ts
+++ src/company/entities/circuitBreaker.entities.ts
@@ -33,28 +33,16 @@ export class CircuitBreaker {
   @Column()
   floorPriceBlockMakret: string;
 
-<<<<<<< HEAD
-  // @Column({ primary: true, type: 'date' })
-  // // @PrimaryColumn({ type: 'date' })
-  // created_at: Date;
-
-  @Column({ primary: true, type: 'timestamp' })
-=======
   @Column({ primary: true, type: 'date' })
   // @PrimaryColumn({ type: 'date' })
   created_at: Date;
 
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
->>>>>>> master
   // @PrimaryColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   updated_at: Date;
 
   @BeforeInsert()
   setPrimaryKey() {
-<<<<<<< HEAD
-    this.updated_at = new Date();
-=======
     this.created_at = new Date();
->>>>>>> master
   }
 }
